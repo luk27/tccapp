cb48844af3e57b776555ba5ddda35a68c508f8ed
luk27 <lukspn.27@gmail.com>
2011-03-27 11:22:55 -0300

    1c

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tccapp::Application.config.secret_token = 'eb35cbe2d16ca655b231b60eef0732614f67129131db5216e70a0dd5b099122f249bf58b8e8273a24eb57960f90c14e88e9171b020395c06106435509c85f688'
