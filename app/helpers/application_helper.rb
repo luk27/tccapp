4b25bf6a194272427168281777f33c1ba97a6d25
luk27 <lukspn.27@gmail.com>
2011-05-08 19:30:09 -0300

    Facebook OK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,11 @@ module ApplicationHelper
 
 include TweetButton
 
+          def facebook_like
+            content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like, :width => 300, :height => 90
+            
+          end
+
   def title
     base_title = "TCC-APP"
     if @title.nil?
